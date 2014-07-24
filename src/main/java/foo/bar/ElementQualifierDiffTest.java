18a04158672dffd05f107aefef8829315a2b6049
Sergey.Titkov <Sergey.Titkov@billing.ru>
2014-07-24 19:09:58 +0400

    Рабочий вариант сравнения двух XML + шаблон XSLT для сортировки.

--- src/main/java/foo/bar/ElementQualifierDiffTest.java
+++ src/main/java/foo/bar/ElementQualifierDiffTest.java
@@ -4,32 +4,77 @@ import org.custommonkey.xmlunit.*;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.net.URL;
+import java.io.*;
 import java.util.List;
 
 public class ElementQualifierDiffTest {
 
+  private static String readResourceToString(String resourceName) throws IOException {
+      String result = "";
+      // Прочитать ресурс в строку, везде кодировка UTF-8
+      try (BufferedReader bufferedReader = new BufferedReader(new FileReader(ElementQualifierDiffTest.class.getResource(resourceName).getPath()))) {
+        StringBuffer stringBuffer = new StringBuffer();
+        String currentLine = "";
+        while ((currentLine = bufferedReader.readLine()) != null) {
+          stringBuffer.append(currentLine).append("\n");
+        }
+        result = stringBuffer.toString();
+      }
+    return result;
+  }
+
   public static void main(String[] args) {
-    URL url1 = ElementQualifierDiffTest.class.getResource("/Structurally-Etalon.XML");
-    URL url2 = ElementQualifierDiffTest.class.getResource("/Structurally-Test.XML");
-    FileReader fr1 = null;
-    FileReader fr2 = null;
+    String template = "";
+    String etalonXML = "";
+    String testXML = "";
+
     try {
-      fr1 = new FileReader(url1.getPath());
-      fr2 = new FileReader(url2.getPath());
-    } catch (FileNotFoundException e) {
+      etalonXML = readResourceToString("/Structurally-Etalon.XML");
+      testXML = readResourceToString("/Structurally-Test.XML");
+      template = readResourceToString("/Table-Sort.XSLT");
+    } catch (IOException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
+
+    // Производим сортировку для эталонного XML.
+    try {
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      StringWriter stringWriter = new StringWriter();
+      Transformer transformer = transformerFactory
+        .newTransformer(new javax.xml.transform.stream.StreamSource(new StringReader(template)));
+      transformer.transform(
+        new javax.xml.transform.stream.StreamSource(new StringReader(readResourceToString("/Structurally-Etalon.XML"))),
+        new javax.xml.transform.stream.StreamResult(stringWriter)
+      );
+      etalonXML = stringWriter.toString();
+    } catch (TransformerConfigurationException e) {
+      e.printStackTrace();
+    } catch (TransformerException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
+
+    // Производим сортировку для тестового XML.
+    try {
+      TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      StringWriter stringWriter = new StringWriter();
+      Transformer transformer = transformerFactory
+        .newTransformer(new javax.xml.transform.stream.StreamSource(new StringReader(template)));
+      transformer.transform(
+        new javax.xml.transform.stream.StreamSource(new StringReader(testXML)),
+        new javax.xml.transform.stream.StreamResult(stringWriter)
+      );
+      testXML = stringWriter.toString();
+    } catch (TransformerConfigurationException e) {
+      e.printStackTrace();
+    } catch (TransformerException e) {
       e.printStackTrace();
     }
+
 
     XMLUnit.setIgnoreComments(Boolean.TRUE);
     XMLUnit.setIgnoreWhitespace(Boolean.TRUE);
@@ -39,19 +84,19 @@ public class ElementQualifierDiffTest {
     XMLUnit.setCompareUnmatched(Boolean.FALSE);
 
     try {
-      Diff diff = new Diff(fr1, fr2);
+      Diff diff = new Diff(new StringReader(etalonXML), new StringReader(testXML));
       System.out.println("Similar? " + diff.similar());
       System.out.println("Identical? " + diff.identical());
 
 
-        DetailedDiff detDiff = new DetailedDiff(diff);
-        List differences = detDiff.getAllDifferences();
-        for (Object object : differences) {
-          Difference difference = (Difference)object;
-          System.out.println("***********************");
-          System.out.println(difference);
-          System.out.println("***********************");
-        }
+      DetailedDiff detDiff = new DetailedDiff(diff);
+      List differences = detDiff.getAllDifferences();
+      for (Object object : differences) {
+        Difference difference = (Difference) object;
+        System.out.println("***********************");
+        System.out.println(difference);
+        System.out.println("***********************");
+      }
 
       /*
       DetailedDiff detDiff = new DetailedDiff(diff);
